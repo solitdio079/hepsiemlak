964690647df1c5b7af75087c3440c6c0b457e8ac
solitdio079 <solitdio079@gmail.com>
2025-01-05 20:16:53 +0300

    country filter

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -1,7 +1,19 @@
 import { useState } from "react"
-import { Form } from "react-router-dom"
+import { Form, Link } from "react-router-dom"
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 export default function Home() {
   const [country, setCountry] = useState(null)
+
     return (
       <div
         className="hero min-h-96"
@@ -92,6 +104,33 @@ export default function Home() {
                 </div>
               </div>
             </Form>
+            <Swiper
+              // install Swiper modules
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={50}
+              slidesPerView={3}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log('slide change')}
+              className="w-full"
+            >
+              <SwiperSlide>
+                <Link className="badge badge-outline">Appart à louer</Link>
+              </SwiperSlide>
+              <SwiperSlide>
+                {' '}
+                <Link className="badge badge-outline">Appart à vendre</Link>
+              </SwiperSlide>
+              <SwiperSlide>
+                {' '}
+                <Link className="badge badge-outline">Bureau à louer</Link>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Link className="badge badge-outline">Appart à vendre</Link>
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
       </div>
