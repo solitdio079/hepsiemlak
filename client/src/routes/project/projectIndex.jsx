b9077e14cf17c49f5acb99d26bc49515f7258ba8
solitdio079 <solitdio079@gmail.com>
2025-01-05 13:50:20 +0300

    added fixed

--- client/src/routes/project/projectIndex.jsx
+++ client/src/routes/project/projectIndex.jsx
@@ -11,7 +11,7 @@ export default function ProjectIndex() {
       <InfiniteEntity
         loaderRoute={`/loaders/allProjectLoader`}
         UnitEntity={ProjectCard}
-        fetchMoreURL={url + `/project/`}
+        fetchMoreURL={url + `/projects/`}
       />
     </div>
   )
