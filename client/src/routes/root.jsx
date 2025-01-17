d7ff9419c49c25c397ac7225df9617011dc38263
solitdio079 <solitdio079@gmail.com>
2025-01-17 16:04:07 +0300

    app changes cookies

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -15,7 +15,7 @@ export async function loader() {
             }
         })
         const response = await req.json()
-        //console.log(document.cookie)
+        console.log(document.cookie)
         if(response.error) return null
         return response
     } catch (error) {
