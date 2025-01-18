963b79900d37dadd5a1d7c099e305c954831de4e
solitdio079 <solitdio079@gmail.com>
2025-01-19 00:34:48 +0300

    see user

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -24,6 +24,7 @@ export async function loader() {
 }
 export default function Root() {
     const user = useLoaderData()
+    alert("user: "+ user)
     return (<userContext.Provider value={user}>
         <Navbar user={user} />
         <Outlet/>
