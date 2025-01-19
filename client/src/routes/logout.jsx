9acc8d5cc7d0647120a843de0238654a5044299e
solitdio079 <solitdio079@gmail.com>
2025-01-19 18:36:08 +0300

    added token auth

--- client/src/routes/logout.jsx
+++ client/src/routes/logout.jsx
@@ -1,6 +1,7 @@
 import { redirect } from 'react-router-dom'
 import { url } from '../utils/serverUrl'
 export async function action() {
+  localStorage.removeItem('token')
   try {
     const req = await fetch(url + '/auth/logout', {
       method: 'POST',
@@ -10,6 +11,7 @@ export async function action() {
         'Content-Type': 'application/json',
       },
     })
+    
     const response = await req.json()
     if (response.error) {
       console.log(response.error)
