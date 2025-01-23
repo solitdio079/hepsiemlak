5779b288bdc11e34fe78c3abf20a5334ccf15d65
solitdio079 <solitdio079@gmail.com>
2025-01-23 21:36:44 +0300

    footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
       <>
         <footer className="footer bg-neutral text-neutral-content items-center p-4">
           <aside className="grid-flow-col items-center">
-            <img width={36}  src="/logo.png" />
+            <img width={48}  src="/footer.png" />
             <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
           </aside>
           <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
